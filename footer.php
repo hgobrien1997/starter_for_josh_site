d637cccda308dc78e2662d7ff04989c2ff76ff08
hgobrien1997 <101072623+hgobrien1997@users.noreply.github.com>
2022-03-18 16:24:30 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,25 @@
+<footer class="container-fluid">
+    <div class="container">
+<div class="row">
+    <div class="col-md-6">
+        <p> JOSH WHITKEN
+
+        </p>
+    </div>
+
+    <div class="col-md-6">
+        <p> Terms and Conditions
+        </p>
+    </div> 
+
+
+</div><!-- row -->
+    </div> <!-- container -->
+</footer> <!-- container-fluid -->
+
+</section>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
