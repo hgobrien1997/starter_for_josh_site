3bfd806e254f29946c1762c3f4e1fb2f132215e4
hgobrien1997 <101072623+hgobrien1997@users.noreply.github.com>
2022-04-13 13:50:23 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -10,6 +10,14 @@ wp_enqueue_style( 'test', get_stylesheet_directory_uri() . '/responsive.css' );
 }
 add_action( 'wp_enqueue_scripts', 'load_my_scripts');
 ?>
+
+<?php
+function register_navwalker(){
+require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+}
+add_action( 'after_setup_theme', 'register_navwalker' );
+?>
+
 <?php
 register_nav_menus( array(
     'primary' => __( 'Primary Menu', 'top-menu' ),
