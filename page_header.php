f387ac6d402b23eb65d7ec5aa4a7fd28a196e274
hgobrien1997 <101072623+hgobrien1997@users.noreply.github.com>
2022-04-13 12:22:33 +0800

    Update page_header.php

--- page_header.php
+++ page_header.php
@@ -70,12 +70,9 @@ echo ' - page '. $paged; }
 
       <div class="smallpage-tag">
 
-<<<<<<< HEAD
-    </div>    <h1 class="smallpage-title"> Projects  </h1>
-=======
-      <h1 class="smallpage-title"> Projects </h1>
-    </div>
->>>>>>> 8547d792c97557164ef1f554920bb6b87c2dbfd8
+    </div>    
+    
+    <h1 class="smallpage-title"> Projects  </h1>
     </div> <!-- container -->
 
     <?php wp_head(); ?>
