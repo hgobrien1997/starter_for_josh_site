88b1be7dc5f9c22679c317c41dc38801f97be2ef
hgobrien1997 <101072623+hgobrien1997@users.noreply.github.com>
2022-04-13 13:03:27 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -10,3 +10,9 @@ wp_enqueue_style( 'test', get_stylesheet_directory_uri() . '/responsive.css' );
 }
 add_action( 'wp_enqueue_scripts', 'load_my_scripts');
 ?>
+
+<?php
+register_nav_menus( array(
+    'primary' => __( 'Primary Menu', 'top-menu' ),
+) );
+?>
